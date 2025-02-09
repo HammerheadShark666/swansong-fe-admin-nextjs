06b100b7d49aa74ab8e576aecb73f6f2ff747a6d
HammerheadShark666 <103519731+HammerheadShark666@users.noreply.github.com>
2025-02-09 19:11:55 +0000

    Implement forgotten password

---
+++ app/(auth)/forgotten-password/page.tsx
@@ -0,0 +1,11 @@
+import ForgottenPasswordForm from "./components/forgottenPasswordForm";
+  
+const ForgottenPasswordPage = () => {
+ 
+  return (
+    <ForgottenPasswordForm></ForgottenPasswordForm> 
+  );
+};
+
+
+export default ForgottenPasswordPage; 
